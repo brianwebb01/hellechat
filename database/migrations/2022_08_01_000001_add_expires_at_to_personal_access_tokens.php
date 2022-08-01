7cd8bbfa34375624357ad5193efc38d353aae169
Brian Webb <brian@brianwebb.org>
2022-08-01 13:18:37 -0400

    sanctum migration fix

--- database/migrations/2022_08_01_000001_add_expires_at_to_personal_access_tokens.php
+++ database/migrations/2022_08_01_000001_add_expires_at_to_personal_access_tokens.php
@@ -13,14 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamps();
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->timestamp('expires_at')->nullable();
         });
     }
 
